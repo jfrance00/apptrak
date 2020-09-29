611bdfcc3b26834cfc84380ca432c085117afb6c
jfrance00 <jfrance00@gmail.com>
2020-09-29 19:09:21 +0300

    changed user loader back

--- apptrak/blueprints/auth/models.py
+++ apptrak/blueprints/auth/models.py
@@ -7,7 +7,7 @@ from ..jobapps.models import JobApplication
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.filter_by(id=user_id).first()
+    return User.query.get(user_id)
 
 
 class User(UserMixin, db.Model):
